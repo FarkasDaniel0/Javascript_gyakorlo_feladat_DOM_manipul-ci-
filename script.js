791a26c51650a16e6922bc1c224089c7daee3978
Farkas Dániel <farkas.dano@gmail.com>
2021-04-25 19:30:48 +0200

    táblázat hozzáadása

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 
 let index = 0;
+var test = "1";
+document.getElementById("myInput").value = test;
 
 let btnAdd = document.querySelector("#add");
+
+
 
 function addHeader() {
   let mainDiv = document.querySelector(".vh-100");
   let newHeader = document.createElement("h1");
-  newHeader.innerText = "Szia Javascript!";
+  // newHeader.innerText = test;
   mainDiv.appendChild(newHeader);
+  function addRow(tableID) {
+    // Get a reference to the table
+    let tableRef = document.getElementById(tableID);
+  
+    // Insert a row at the end of the table
+    let newRow = tableRef.insertRow(-1);
+  
+    // Insert a cell in the row at index 0
+    let newCell = newRow.insertCell(0);
+  
+    // Append a text node to the cell
+    let newText = document.createTextNode(test);
+    newCell.appendChild(newText);
+  }
+  
+  // Call addRow() with the table's ID
+  addRow('my-table');
   index++;
 }
 
@@ -20,8 +41,13 @@ function deleteChild() {
     let mainDiv = document.querySelector(".vh-100");
     let lastChild = mainDiv.lastChild;
     mainDiv.removeChild(lastChild);
+    let table = document.querySelector('table');
+
+// Delete second row
+table.deleteRow(1);
     index--;
   }
 }
 
 btnDelete.addEventListener("click", deleteChild);
+
